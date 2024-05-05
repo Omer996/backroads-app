d8f17baf26e085f245bda0d6815a9a656e0a0154
Omer996 <125209202+Omer996@users.noreply.github.com>
2024-05-05 13:32:47 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
+import Tours from './components/Tours';
+
+
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <About />
+      <Services/>
+      <Tours/>
+      <Footer/>
+
+
+    </>
+  );
+}
+
+export default App;
